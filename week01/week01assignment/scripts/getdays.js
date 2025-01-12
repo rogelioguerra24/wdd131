83d549da1e68f3ffc3f8e3da506609e1f02a9276
rogelioguerra24 <gue24018@byui.edu>
2025-01-11 23:32:52 -0500

    new page

---
+++ week01/week01assignment/scripts/getdays.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+const year = document.querySelector("#currentyear");
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastModified = document.lastModified;
+const last = document.querySelector("#lastModified")
+last.innerHTML = `${lastModified}`;
