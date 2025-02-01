92cb0dc8eeb4781b0306c77032ac712851ed5f8f
rogelioguerra24 <gue24018@byui.edu>
2025-02-01 16:45:08 -0500

    New page in system

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = document.querySelector("#currentyear");
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastModified = document.lastModified;
+const last = document.querySelector("#lastModified")
+last.innerHTML = `${lastModified}`;
+/*comment*/
